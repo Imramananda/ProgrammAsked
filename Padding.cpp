2565f3c27c9e73b8b3f6501aa6a0425390bce877
Imramananda <ramanandayadav@gmail.com>
2025-10-15 14:52:44 +0530

    Padding issue in structure

--- Padding.cpp
+++ Padding.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<print>
 #include<vector>
-
+using namespace std;
 struct inEff
 {
     char a;
@@ -21,6 +21,13 @@ int main()
 {
     std::cout<<"Memory for inEff in byte "<<sizeof(inEff)<<std::endl;
     std::cout<<"Memory for eff in byte "<<sizeof(Eff)<<std::endl;
+    vector<inEff>iv(10000000);
+    vector<Eff>v(10000000);
+    const double iEfff = sizeof(inEff)*iv.size();
+    std::cout<<"Memory for inEff in byte "<<iEfff/(1024*1024)<<std::endl;
+    const double Efff = sizeof(Eff)*v.size();
+    std::cout<<"Memory for eff in byte "<<sizeof(Efff)/(1024*1024)<<std::endl;
+    
     return 0;
 }
 
